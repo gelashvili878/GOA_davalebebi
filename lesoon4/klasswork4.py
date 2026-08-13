031b69f7157553015c02a9e037bc4ceb9940650b
gelashvili878 <tazogelashvili23@gmail.com>
2026-08-13 18:13:55 +0400

    second commit

---
+++ lesoon4/klasswork4.py
@@ -0,0 +1,53 @@
+print(39+37)
+print(79+99)
+print(177+259)
+
+print(298-177)
+print(979-386)
+print(689-197)
+
+print(39*37)
+print(247*99)
+print(178*260)
+
+print(298/17)
+print(98/3)
+print(395/5)
+
+print(39//7)
+print(247/99)
+print(178//26)
+
+print(29%17)
+print(46%9)
+print(72%9)
+
+num1=39
+num2=47
+num3=98
+print(num1+num2+num3)
+
+length=20
+width=10
+perimeter=2*(length+width)
+print(perimeter)
+
+name="TAZO"
+print(name)
+username="gelaShvili"
+print(username)
+age=15
+print(age)
+height=1.83
+print(height)
+studying="goal-oriented academy"
+print(studying)
+lives_in="surami"
+print(lives_in)
+
+print("my name is",+"TAZo")
+print("usernames is",+"gelashvili")
+print("age is",+15)
+print("height is",+1.83)
+print("studying in,",+"goal_oriented academy")
+print("lives in",+"surami")
---
+++ lesoon7/classwork8.py
@@ -0,0 +1,25 @@
+my_age=int(input("enter MY AGE 15 "))
+
+#input არის ინსტრუქცია მას შემოაქვს ინფორმაცია ჩვენს კოდში ან კომპიუტერში
+#output არის ამ იმფორმაციის შედეგი, ხოლო PRINT() ფუნქციის დახმარებით ჩვენ შეგვიძლია დავინახოთ ინფუთის აუთფუთი
+
+NUM1=int(input(" enter number20"))
+NUM2=int(input("enter number 10 "))
+PRINT=(NUM1+NUM2)
+PRINT=(NUM1-NUM2)
+PRIN=(NUM1*NUM2)
+PRINT=(NUM1/NUM2)
+
+MY_FAVORITE_COLOR=(input("enter BLACK"))
+MY_FAVORITE_ANIMAL=(input("enter your DOG"))
+MY_FAVORITE_PLACE=(input("enter your BATUMI"))
+PRINT="MY FAVORITE IS:COLOR:"+"BLACK","ANIMAL:"+"DOG","PLACE:"+"BATUMI"
+
+BMW_MODEL=(input("enter bmw x8"))
+print("MY FAVORITE MODEL IS "+"bmw x8 ")
+MERSEDES_MODEL=(input("enter mercedes s class"))
+print("MY FAVORITE MODEL IS "+"mersedes s class")
+bugatti_model=(input("enter bugatti chiron"))
+print("my favorite model is "+"bugatti chiron")
+maclaren_model=(input("enter maclaren p1"))
+print("my favorite model is "+"maclaren p1")

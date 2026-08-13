41d7963ce74baff29ddb11100097eccf83c2058a
gelashvili878 <tazogelashvili23@gmail.com>
2026-08-13 18:10:13 +0400

    first commit

---
+++ lesoon3/klasswork3.py
@@ -0,0 +1,32 @@
+print(6 + 6)
+print(21+21)
+
+print(100-78)
+print(33-11)
+
+print(7 * 6)
+print(8 * 7)
+
+print(68/4)
+print(56/8)
+
+print(54//6)
+print(22//7)
+
+print(24%5)
+print(37%9)
+
+name = "TAZO"
+print(name)
+age = 15
+print(age)
+color = "black"
+print(color)
+food = "kinkale"
+print(food)
+car = "bugatti"
+print(car)
+
+print("MY sister name is " + "mari")
+print("my favorite subject is " + "math")
+print(" I live in " + "surami")

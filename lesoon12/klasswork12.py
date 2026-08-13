317f6f767e8bd3f9030595e2f610450822cfe2c1
gelashvili878 <tazogelashvili23@gmail.com>
2026-08-13 18:15:10 +0400

    second commit

---
+++ lesoon12/klasswork12.py
@@ -0,0 +1,31 @@
+#logical operators- არის or და and 
+
+print(15>27)
+print(46<=32)
+print(59>77)
+print(32>=98)
+print(99==32)
+
+#მე ვიყიდე ყველი და პური
+#მარტო ყველს თუ იყიდი false
+#მარტო პურს თუ იყიდი false
+#ყველსაც და პურსაც თუ იყიდი true
+#ყველსაც და პურსაც არ იყიდი false
+
+#მე ვიყიდი ყველს ან პურს true
+#მარტო ყველს თუ იყიდი true
+#მარტო პურს თუ იყიდი true
+#ყველსაც და პურსაც თუ იყიდი true
+# არც ყველს და არც პურს როცა არ იყიდი false
+ 
+#print(false and true) false
+#print(true and true) true 
+#print(false and false) false
+
+#print(false or false) false
+#print(true or false) true
+#print(true or true) true
+
+num=10
+while num > 0:
+    print=(0,10,1)
